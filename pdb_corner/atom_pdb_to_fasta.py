0767063777ba41476a8130600dbe69a2e7255174
Olga Tsiouri <olgatsiouri@outlook.com>
2020-10-02 14:32:54 +0300

    Update atom_pdb_to_fasta.py

--- pdb_corner/atom_pdb_to_fasta.py
+++ pdb_corner/atom_pdb_to_fasta.py
@@ -3,7 +3,7 @@ import argparse
 from Bio import SeqIO
 # input parameters
 ap = argparse.ArgumentParser()
-ap.add_argument("-in", "--input_file", required=True, help="input pdb file without SEQRES header")
+ap.add_argument("-in", "--input_file", required=True, help="input pdb file without SEQRES header(you should add a header if you want your output fasta file to have one otherwise ?? apears on the fasta header line)")
 ap.add_argument("-out", "--output_file", required=True, help="output fasta file")
 args = vars(ap.parse_args())
 # main
