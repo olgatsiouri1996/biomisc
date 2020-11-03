9ba1fb0ce323e956e50883d43214b7aac574b55b
Olga Tsiouri <olgatsiouri@outlook.com>
2020-11-03 12:30:19 +0200

    Update print_or_remove_lines_by_pattern.R

--- text_manipulation/print_or_remove_lines_by_pattern.R
+++ text_manipulation/print_or_remove_lines_by_pattern.R
@@ -2,7 +2,7 @@
 library(glue)
 library(stringr)
 args = commandArgs(trailingOnly=TRUE)
-glue("usage: print_or_remove_lines_by_pattern.R input_txt column_name pattern boolean_print_or_remove output_txt")
+glue("usage: print_or_remove_lines_by_pattern.R input_txt header_boolean column_name pattern boolean_print_or_remove output_txt")
 # main
 input_txt <- read.delim(args[1], header= as.logical(args[2]))
 filtered_txt <- input_txt[str_detect(input_txt[ ,as.numeric(args[3])], args[4], negate = as.logical(args[5])), ] 
