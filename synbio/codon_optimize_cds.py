e7801f83300de8e47c6ed19dd9c4e68ca4bead9c
Olga Tsiouri <olgatsiouri@outlook.com>
2021-07-30 09:39:57 +0300

    output in fasta format instead of genbank

--- synbio/codon_optimize_cds.py
+++ synbio/codon_optimize_cds.py
@@ -2,16 +2,19 @@
 import argparse
 from Bio import SeqIO
 from dnachisel import *
+import sys
 # imput parameters
 ap = argparse.ArgumentParser()
-ap.add_argument("-fa", "--fasta", required=True, help="input fasta file")
+ap.add_argument("-fa", "--fasta", required=True, help="input single or multi fasta file")
 ap.add_argument("-org","--organism", required=True, help="organism to input(use either the names of the genomes avaliable on dnachisel or use the taxid of the organisms in http://www.kazusa.or.jp/codon/)")
-ap.add_argument("-id","--seqid", required=True, help="sequence name")
-ap.add_argument("-opt","--optimized", required=True, help="optimized genbank file")
+ap.add_argument("-opt","--optimized", required=True, help="optimized fasta file")
 args = vars(ap.parse_args())
 # main
 for record in SeqIO.parse(args['fasta'], "fasta"):
     problem = DnaOptimizationProblem(sequence=str(record.seq),
     objectives=[CodonOptimize(species= args['organism'])])
     problem.optimize()
-final_record = problem.to_record(args['optimized'])
+    sys.stdout = open(args['optimized'], 'a')
+    print(">"+record.id,problem.sequence, sep='\n')
+    sys.stdout.close()
+
