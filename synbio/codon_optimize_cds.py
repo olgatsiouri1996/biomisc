025847616e5e51fad85220497c07d91fa30c124b
Olga Tsiouri <olgatsiouri@outlook.com>
2021-07-30 12:05:40 +0300

    add sys before for loop

--- synbio/codon_optimize_cds.py
+++ synbio/codon_optimize_cds.py
@@ -5,16 +5,16 @@ from dnachisel import *
 import sys
 # imput parameters
 ap = argparse.ArgumentParser()
-ap.add_argument("-fa", "--fasta", required=True, help="input single or multi fasta file")
+ap.add_argument("-fa", "--fasta", required=True, help="input fasta file")
 ap.add_argument("-org","--organism", required=True, help="organism to input(use either the names of the genomes avaliable on dnachisel or use the taxid of the organisms in http://www.kazusa.or.jp/codon/)")
-ap.add_argument("-opt","--optimized", required=True, help="optimized fasta file")
+ap.add_argument("-opt","--optimized", required=True, help="optimized genbank file")
 args = vars(ap.parse_args())
 # main
+sys.stdout = open(args['optimized'], 'a')
 for record in SeqIO.parse(args['fasta'], "fasta"):
     problem = DnaOptimizationProblem(sequence=str(record.seq),
     objectives=[CodonOptimize(species= args['organism'])])
     problem.optimize()
-    sys.stdout = open(args['optimized'], 'a')
     print(">"+record.id,problem.sequence, sep='\n')
-    sys.stdout.close()
+sys.stdout.close()
 
