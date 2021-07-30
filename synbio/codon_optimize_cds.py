6a5489f8571bfda2863642fd8f426742fa381830
Olga Tsiouri <olgatsiouri@outlook.com>
2021-07-30 12:16:10 +0300

    output in fasta instead of genbank

--- synbio/codon_optimize_cds.py
+++ synbio/codon_optimize_cds.py
@@ -5,9 +5,9 @@ from dnachisel import *
 import sys
 # imput parameters
 ap = argparse.ArgumentParser()
-ap.add_argument("-fa", "--fasta", required=True, help="input fasta file")
+ap.add_argument("-fa", "--fasta", required=True, help="input single or multi fasta file")
 ap.add_argument("-org","--organism", required=True, help="organism to input(use either the names of the genomes avaliable on dnachisel or use the taxid of the organisms in http://www.kazusa.or.jp/codon/)")
-ap.add_argument("-opt","--optimized", required=True, help="optimized genbank file")
+ap.add_argument("-opt","--optimized", required=True, help="optimized fasta file")
 args = vars(ap.parse_args())
 # main
 sys.stdout = open(args['optimized'], 'a')
