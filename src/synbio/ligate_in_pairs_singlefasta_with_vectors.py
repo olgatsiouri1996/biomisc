148c5a3e0bf8e037caaf40485a8ad662df456d96
Olga Tsiouri <olgatsiouri@outlook.com>
2022-02-11 19:24:59 +0200

    change the script to support file lists with duplicates

--- src/synbio/ligate_in_pairs_singlefasta_with_vectors.py
+++ src/synbio/ligate_in_pairs_singlefasta_with_vectors.py
@@ -13,47 +13,25 @@ args = vars(ap.parse_args())
 # main
 # inport txt file and convert each column to list
 df_txt = pd.read_csv(args['txt_file'], sep="\t")
+gb_list = df_txt.iloc[:,0].values.tolist()
+fasta_list = df_txt.iloc[:,1].values.tolist()
 # create lists
 gb_seqs = []
 fasta_seqs = []
-fasta_ids = []
-gb_ids = []
 gb_features = []
 # linear vectors
-# import each genbank file from the working directory
-for filename in sorted(os.listdir(str(os.getcwd()))):
-    if filename.endswith(".gb") or filename.endswith(".gbk"):
-        plasmid = SeqIO.read(filename, "genbank")
-        gb_seqs.append(str(plasmid.seq))
-        gb_features.append(plasmid.features)
-        gb_ids.append(str(filename))
-# create dataframe and add the lists on it
-df_gb = pd.DataFrame()
-df_gb['genbank'] = gb_ids
-df_gb['genbank_seqs'] = gb_seqs
-df_gb['genbank_features'] = gb_features
+# import each genbank file from the list
+for i in gb_list:
+    plasmid = SeqIO.read(gb_list[gb_list.index(i)], "genbank")
+    gb_seqs.append(str(plasmid.seq))
+    gb_features.append(plasmid.features)
 # DNA insert
-# import each fasta file from the working directory
-for filename in sorted(os.listdir(str(os.getcwd()))):
-    if filename.endswith(".fa") or filename.endswith(".fasta"):
-        record = SeqIO.read(filename, "fasta")
-        fasta_seqs.append(str(record.seq))
-        fasta_ids.append(str(filename))
-# create dataframe and add the lists on it
-df_fa = pd.DataFrame()
-df_fa['fasta'] = fasta_ids
-df_gb['fasta_seqs'] = fasta_seqs
-# merge data frames
-df_full = df_txt.join(df_gb.set_index('genbank'), on='genbank')
-df_full = df_full.join(df_fa.set_index('fasta'), on='fasta')
-# convert to list
-gb_list = df_full.iloc[:,0].values.tolist()
-fasta_list = df_full.iloc[:,1].values.tolist()
-genbank_sequences = df_full.iloc[:,2].values.tolist()
-genbank_features = df_full.iloc[:,3].values.tolist()
-fasta_sequences = df_full.iloc[:,4].values.tolist()
+# import each fasta file from the list
+for i in fasta_list:
+    record = SeqIO.read(fasta_list[fasta_list.index(i)], "fasta")
+    fasta_seqs.append(str(record.seq))
 # iterate all below lists in pairs
-for (a,b,c,d,e) in zip(gb_list,fasta_list,genbank_sequences,genbank_features,fasta_sequences):
+for (a,b,c,d,e) in zip(gb_list,fasta_list,gb_seqs,gb_features,fasta_seqs):
     # merge
     seqad = str(c) + str(e)
     # add this record to the list
