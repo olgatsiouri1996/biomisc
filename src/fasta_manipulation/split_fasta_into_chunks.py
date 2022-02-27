66c2784a5a27388d47a352b5605bcf1905b5c5bd
Olga Tsiouri <olgatsiouri@outlook.com>
2022-02-27 18:37:31 +0200

    script to split fasta into chunks is added

---
+++ src/fasta_manipulation/split_fasta_into_chunks.py
@@ -0,0 +1,30 @@
+# python3
+import argparse
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+# input parameters
+ap = argparse.ArgumentParser()
+ap.add_argument("-in", "--input_file", required=True, help="input single or multi-fasta file")
+ap.add_argument("-out", "--output_file", required=False, help="output multi-fasta file")
+ap.add_argument("-step", "--step_size", required=True, help="step size for chunk creation, type = integer")
+ap.add_argument("-win", "--window_size", required=True, help="window size for chunk creation, type = integer")
+ap.add_argument("-pro", "--program", type=int, default=1, required=False, help="program output to select 1) 1 multi-fasta file 2) many single-fasta files. Default is 1")
+args = vars(ap.parse_args())
+# main
+seqs = []
+headers = [] 
+chunks = []# setup empty lists
+# import multi or single-fasta file
+for record in SeqIO.parse(args['input_file'], "fasta"):
+	for i in range(0, len(record.seq) - int(args['window_size']) + 1, int(args['step_size'])):
+		seqs.append(record.seq[i:i + int(args['window_size'])])
+		headers.append('_'.join([record.id,str(i)]))
+# export to multi or single-fasta
+for (seq, header) in zip(seqs,headers):
+	if args['program']==1:
+		chunks.append(SeqRecord(Seq(str(seq)),id=str(header),description=""))
+		SeqIO.write(chunks,args['output_file'], "fasta")
+	else:
+		chunk = SeqRecord(Seq(str(seq)),id=str(header),description="")
+		SeqIO.write(chunk, ''.join([str(header),".fasta"]), "fasta")
