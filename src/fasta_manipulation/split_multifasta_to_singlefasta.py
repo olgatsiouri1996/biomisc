47dc916514ac73cfeb6303e3f3a2144430679a44
Olga Tsiouri <olgatsiouri@outlook.com>
2022-04-23 04:11:58 +0300

    optimize for larger files

--- src/fasta_manipulation/split_multifasta_to_singlefasta.py
+++ src/fasta_manipulation/split_multifasta_to_singlefasta.py
@@ -1,17 +1,23 @@
 # python3
 import os
+import sys
 import argparse
-from Bio import SeqIO
-from Bio.SeqRecord import SeqRecord
-from Bio.Seq import Seq
+from pyfaidx import Fasta
 # imput parameters
 ap = argparse.ArgumentParser()
 ap.add_argument("-mfa", "--multifasta", required=True,  help="input multi-fasta file to split to single-fasta")
 ap.add_argument("-dir", "--directory", required=False, default='.', type=str, help="output directory to save the single-fasta files. Default is the current directory")
 args = vars(ap.parse_args())
 # main
-# set working directory
-records = SeqIO.parse(args['multifasta'], "fasta")
+# create function to split the input sequence based on a specific number of characters(60)
+def split_every_60(s): return [str(s)[i:i+60] for i in range(0,len(str(s)),60)]
+# import multi-fasta file
+features = Fasta(args['multifasta'])
+# select output directory
 os.chdir(args['directory'])
-for record in records:
-	SeqIO.write(record, ''.join([record.id,".fasta"]), "fasta")
+# export each record to a single-fasta    
+for key in features.keys():
+    sys.stdout = open(''.join([str(key),".fasta"]), 'a')
+    print(''.join([">",str(key)]))
+    print('\n'.join(split_every_60(features[str(key)][:].seq)))
+    sys.stdout.close()
