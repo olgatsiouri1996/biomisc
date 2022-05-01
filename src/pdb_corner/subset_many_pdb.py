a14cc79d70e416528e19a3f9985842e66cc74e77
Olga Tsiouri <olgatsiouri@outlook.com>
2022-05-01 18:12:32 +0300

    use relative instead of absolute path

--- src/pdb_corner/subset_many_pdb.py
+++ src/pdb_corner/subset_many_pdb.py
@@ -8,9 +8,9 @@ import warnings
 from Bio import BiopythonWarning
 # input parameters
 ap = argparse.ArgumentParser()
-ap.add_argument("-pdb", "--pdbdir", required=False, type=str, default='.', help=" directory containing the input pdb files(absolute path). Defaults to the current directory")
+ap.add_argument("-pdb", "--pdbdir", required=False, type=str, default='.', help=" directory containing the input pdb files(relative path). Defaults to the current directory")
 ap.add_argument("-txt", "--txtfile", required=True, help="4 column tabular txt file with pdb filename, chain, start and end position columns respectively")
-ap.add_argument("-out", "--outdir", required=False, type=str, default='.', help=" directory to save the output pdb files(absolute path). Defaults to the current directory")
+ap.add_argument("-out", "--outdir", required=False, type=str, default='.', help=" directory to save the output pdb files(relative path). Defaults to the current directory")
 args = vars(ap.parse_args())
 # main
 # ignore warnings
@@ -24,12 +24,12 @@ end_pos = df_txt.iloc[:,3].values.tolist()
 # iterate all above lists and subset each pdb file
 for (a, b, c, d) in zip(ids,chains,star_pos,end_pos):
 # set working directory
-	os.chdir(args['pdbdir'])
+	os.chdir(os.path.realpath(args['pdbdir']))
 # import pdb
 	parser = PDBParser()
 	s = parser.get_structure("name", ''.join([str(a),".pdb"]))
 # set working directory to export
-	os.chdir(args['outdir'])
+	os.chdir(os.path.realpath(args['outdir']))
 # supbset and export to pdb
 	Bio.PDB.Dice.extract(s, str(b), int(c), int(d), ''.join([str(a),"_",str(b),"_",str(c),"_",str(d),".pdb"]))
 
