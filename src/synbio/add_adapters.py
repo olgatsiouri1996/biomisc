3c99266fbe44b4418d2a62c47d6fd2d4535282b6
Olga Tsiouri <olgatsiouri@outlook.com>
2022-05-21 22:17:42 +0300

    import adapters via terminal  instead of file

--- src/synbio/add_adapters.py
+++ src/synbio/add_adapters.py
@@ -5,25 +5,15 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 # imput parameters
 ap = argparse.ArgumentParser()
-ap.add_argument("-up", "--upstream", required=True,  help="upstream adapter(fasta format)")
-ap.add_argument("-fa", "--fasta", required=True,  help="input single or multi fasta  file with the sequence/s to add adapters")
-ap.add_argument("-down", "--downstream", required=True,  help="downstream adapter(fasta format)")
-ap.add_argument("-out", "--output", required=True,  help="output fasta file")
+ap.add_argument("-i", "--input", required=True,  help="input single or multi fasta file")
+ap.add_argument("-l", "--left", required=False, type=str, default="",  help="adapter to the left of the sequence. Default is no left adapter sequence to add")
+ap.add_argument("-r", "--right", required=False, type=str, default="",  help="adapter to the right of the sequence. Default is no right adapter sequence to add")
+ap.add_argument("-o", "--output", required=True,  help="output fasta file")
 args = vars(ap.parse_args())
 # main
-# first adapter
-for record in SeqIO.parse(args['upstream'], "fasta"):
-    x = str(record.seq)
-# second adapter
-for record in SeqIO.parse(args['downstream'], "fasta"):
-    z = str(record.seq)
-# sequence
 records = [] # setup an empty list
-for record in SeqIO.parse(args['fasta'], "fasta"):
-    y = str(record.seq)
-# merge
-    seqad = x + y + z
+for record in SeqIO.parse(args['input'], "fasta"):
     # add this record to the list
-    records.append(SeqRecord(Seq(seqad),id=record.id,description=""))
+    records.append(SeqRecord(Seq(''.join([args['left'],str(record.seq),args['right']])),id=record.id,description=record.description))
 # export to fasta
 SeqIO.write(records, args['output'], "fasta")
