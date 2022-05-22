78923e3d4ef07ad79888a03badeb720ec67cc67c
Olga Tsiouri <olgatsiouri@outlook.com>
2022-05-23 02:29:55 +0300

    fasta descriptions for single fasta files, no storing before output

--- src/fasta_manipulation/extract_or_remove_seqs_from_fasta.py
+++ src/fasta_manipulation/extract_or_remove_seqs_from_fasta.py
@@ -1,4 +1,5 @@
 # python3
+import os
 import argparse
 from Bio import SeqIO
 from Bio.Seq import Seq
@@ -9,7 +10,7 @@ ap.add_argument("-in", "--input", required=True, help="input fasta file")
 ap.add_argument("-out", "--output", required=False, help="output fasta file")
 ap.add_argument("-pro", "--program",type=int, default=1, required=False, help="choose to exract or remove( 1) extract, 2) remove, 3) extract and export as single-fasta files, 4) remove and export as single-fasta files. Defaults to 1)")
 ap.add_argument("-headers", "--headers", required=True, help="file with fasta headers to retrieve the output fasta sequences")
-ap.add_argument("-dir", "--directory", required=False, type=str, default='.', help="output directory to save the single-fasta files. Default is the current directory")
+ap.add_argument("-dir", "--directory", required=False, type=str, default='.', help="relative or absolute path to the output directory to save the single-fasta files. Default is the current directory")
 args = vars(ap.parse_args())
 # main
 wanted = set()
@@ -38,19 +39,15 @@ if program == 2:
 if program == 3:
     records = []
     fasta_sequences = SeqIO.parse(open(args['input']),'fasta')
+    os.chdir(args['directory'])
     for seq in fasta_sequences:
         if seq.id in wanted:
-            records.append(SeqRecord(Seq(str(seq.seq)),id=str(seq.id),description=""))
-    os.chdir(args['directory'])
-    for record in records:
-        SeqIO.write(record, "".join([str(record.id),".fasta"]), "fasta")
+            SeqIO.write(seq, "".join([str(seq.id),".fasta"]), "fasta")
 # remove and export as single-fasta files
 if program == 4:
     records = []
     fasta_sequences = SeqIO.parse(open(args['input']),'fasta')
+    os.chdir(args['directory'])
     for seq in fasta_sequences:
         if seq.id not in wanted:
-            records.append(SeqRecord(Seq(str(seq.seq)),id=str(seq.id),description=""))
-    os.chdir(args['directory'])
-    for record in records:
-        SeqIO.write(record, "".join([str(record.id),".fasta"]), "fasta")
+            SeqIO.write(seq, "".join([str(seq.id),".fasta"]), "fasta")
