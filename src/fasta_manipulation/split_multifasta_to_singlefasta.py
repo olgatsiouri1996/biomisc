1940ceadcac3dad99fb769f95e5fbb998f583f1c
Olga Tsiouri <olgatsiouri@outlook.com>
2022-06-13 02:24:32 +0300

    added fasta description for output files

--- src/fasta_manipulation/split_multifasta_to_singlefasta.py
+++ src/fasta_manipulation/split_multifasta_to_singlefasta.py
@@ -18,6 +18,6 @@ os.chdir(args['directory'])
 # export each record to a single-fasta    
 for key in features.keys():
     sys.stdout = open(''.join([str(key),".fasta"]), 'a')
-    print(''.join([">",str(key)]))
+    print(''.join([">",features[str(key)].long_name]))
     print('\n'.join(split_every_60(features[str(key)][:].seq)))
     sys.stdout.close()
