ff19e031c3a2208a9dc058f4b37274067e113324
Olga Tsiouri <olgatsiouri@outlook.com>
2022-06-24 18:29:00 +0300

    new files uploaded

--- src/fasta_manipulation/complement_fasta.py
+++ src/fasta_manipulation/complement_fasta.py
@@ -2,16 +2,15 @@
 import argparse
 from Bio import SeqIO
 # input parameters
-ap = argparse.ArgumentParser()
+ap = argparse.ArgumentParser(description="complement a multi or single-fasta file")
 ap.add_argument("-in", "--input_file", required=True, help="input fasta file")
 ap.add_argument("-out", "--output_file", required=True, help="output fasta file")
 args = vars(ap.parse_args())
 # main
 sequences = []  # setup an empty list
 for record in SeqIO.parse(args['input_file'], "fasta"):
-        # add this record to the list
-    record.seq = record.seq.reverse_complement()
+    # add this record to the list
+    record.seq = record.seq.complement()
     sequences.append(record)
-
 SeqIO.write(sequences, args['output_file'], "fasta")
 
--- src/fasta_manipulation/reverse_fasta.py
+++ src/fasta_manipulation/reverse_fasta.py
@@ -2,16 +2,13 @@
 import argparse
 from Bio import SeqIO
 # input parameters
-ap = argparse.ArgumentParser()
+ap = argparse.ArgumentParser(description="reverse a multi or single-fasta file")
 ap.add_argument("-in", "--input_file", required=True, help="input fasta file")
 ap.add_argument("-out", "--output_file", required=True, help="output fasta file")
 args = vars(ap.parse_args())
 # main
 sequences = []  # setup an empty list
 for record in SeqIO.parse(args['input_file'], "fasta"):
-        # add this record to the list
-    record.seq = record.seq.reverse_complement()
-    sequences.append(record)
-
+    # add this record to the list
+    sequences.append(record[::-1])
 SeqIO.write(sequences, args['output_file'], "fasta")
-
