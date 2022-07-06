bb079065421497c2209c01f88647de4c650107c3
Olga Tsiouri <olgatsiouri@outlook.com>
2022-07-07 02:50:14 +0300

    option to subset based on start and end locations

--- src/pdb_corner/dssp_statistics_by_chain.py
+++ src/pdb_corner/dssp_statistics_by_chain.py
@@ -3,18 +3,21 @@ import argparse
 from Bio.PDB import *
 import pandas as pd
 import os
-# input parameters
+# input paramexters
 ap = argparse.ArgumentParser()
-ap.add_argument("-in", "--input_pdb", required=True, help="input pdb file")
-ap.add_argument("-model", "--pdb_model", required=False, default= 0, help="model from pdb file to select(integer, default=0)")
-ap.add_argument("-chain", "--pdb_chain", required=True, help="chain from pdb file to select")
+ap.add_argument("-in", "--input", required=True, help="input pdb file(every name except: out.pdb)")
+ap.add_argument("-model", "--model", required=False, default= 0, help="model from pdb file to select(integer, default=0)")
+ap.add_argument("-chain", "--chain", required=True, help="chain from pdb file to select")
+ap.add_argument("-start", "--start", required=False, type=int, help="amino acid in chain to start writing the pdb file")
+ap.add_argument("-end", "--end", required=False, type=int, help="amino acid in chain to end writing the pdb file")
+ap.add_argument("-pro", "--program", required=False, type=int, default=1, help="program to choose: 1. select only the model and chain, 2. select model, chain, start and end locations. Default is 1")
 ap.add_argument("-out", "--output", required=True, help="output txt file(every name except: out.pdb)")
 args = vars(ap.parse_args())
 # main
 # select model and chain
 parser = PDBParser()
-s = parser.get_structure("name", args['input_pdb'])
-fill = s[int(args['pdb_model'])][args['pdb_chain']]
+s = parser.get_structure("name", args['input'])
+fill = s[int(args['model'])][args['chain']]
 io = PDBIO()
 io.set_structure(fill)
 io.save("out.pdb")
@@ -25,6 +28,12 @@ fill = s[0]
 dssp = DSSP(fill,"out.pdb", dssp='mkdssp')
 df = pd.DataFrame(dssp)
 df = df.loc[:, 2]
+# choose program
+if args['program'] == 1:
+    pass    
+else:
+    df = df.loc[int(args['start'] -1):int(args['end'] -1)]
+# create list with structures
 struct_list = df.values.tolist()
 df1 = pd.DataFrame()
 df1['struct_list'] = struct_list
