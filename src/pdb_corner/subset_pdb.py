64e8a2560a8e4565a697eadba78ff18555b4928f
Olga Tsiouri <olgatsiouri@outlook.com>
2022-07-07 04:22:37 +0300

    added support for multiple files

--- src/pdb_corner/subset_pdb.py
+++ src/pdb_corner/subset_pdb.py
@@ -1,16 +1,31 @@
 # python3
+import os
 import argparse
 import Bio
 from Bio.PDB import *
+import warnings
+from Bio import BiopythonWarning
 # input parameters
 ap = argparse.ArgumentParser()
-ap.add_argument("-in", "--input_file", required=True, help="input pdb file")
-ap.add_argument("-chain", "--pdb_chain", required=True, help="chain from pdb file to select")
-ap.add_argument("-start", "--chain_start", required=True, type=int, help="amino acid in chain to start writing the pdb file")
-ap.add_argument("-end", "--chain_end", required=True, type=int, help="amino acid in chain to end writing the pdb file")
+ap.add_argument("-in", "--input", required=False, help="input pdb file with 1 model")
+ap.add_argument("-pro", "--program", required=False,default=1, type=int, help="program to choose 1) subset 1 pdb file 2) subset many pdb files. Default is 1")
+ap.add_argument("-chain", "--chain", required=True, help="chain from pdb file to select")
+ap.add_argument("-start", "--start", required=True, type=int, help="amino acid in chain to start writing the pdb file")
+ap.add_argument("-end", "--end", required=True, type=int, help="amino acid in chain to end writing the pdb file")
 args = vars(ap.parse_args())
 #main
-parser = PDBParser()
-s = parser.get_structure("name", args['input_file'])
-Bio.PDB.Dice.extract(s, args['pdb_chain'], args['chain_start'],args['chain_end'], ''.join([args['input_file'].split(".")[0],"_",args['pdb_chain'],"_",str(args['chain_start']),"_",str(args['chain_end']),".pdb"]))
+# ignore warnings
+warnings.simplefilter('ignore', BiopythonWarning)
+# choose program
+if args['program'] == 1:
+    parser = PDBParser()
+    s = parser.get_structure("name", args['input'])
+    Bio.PDB.Dice.extract(s, args['chain'], args['start'],args['end'], ''.join([args['input'].split(".")[0],"_",args['chain'],"_",str(args['start']),"_",str(args['end']),".pdb"]))
+else:
+    # import each fasta file from the working directory
+    for filename in sorted(os.listdir(str(os.getcwd()))):
+        if filename.endswith(".pdb"):
+            parser = PDBParser()
+            s = parser.get_structure("name", filename)
+            Bio.PDB.Dice.extract(s, args['chain'], args['start'],args['end'], ''.join([filename.split(".")[0],"_",args['chain'],"_",str(args['start']),"_",str(args['end']),".pdb"]))
 
--- src/pdb_corner/subset_pdb_by_chain.py
+++ src/pdb_corner/subset_pdb_by_chain.py
@@ -1,4 +1,5 @@
 # python3
+import os
 import argparse
 import Bio
 from Bio.PDB import *
@@ -6,17 +7,30 @@ import warnings
 from Bio import BiopythonWarning
 # input parameters
 ap = argparse.ArgumentParser()
-ap.add_argument("-in", "--input_file", required=True, help="input pdb file with 1 model")
-ap.add_argument("-chain", "--pdb_chain", required=True, help="chain from pdb file to select")
-ap.add_argument("-out", "--output_file", required=True, help="output pdb file")
+ap.add_argument("-in", "--input", required=False, help="input pdb file with 1 model")
+ap.add_argument("-pro", "--program", required=False,default=1, type=int, help="program to choose 1) subset 1 pdb file 2) subset many pdb files. Default is 1")
+ap.add_argument("-chain", "--chain", required=True, help="chain from pdb file to select")
 args = vars(ap.parse_args())
 # main
 # ignore warnings
 warnings.simplefilter('ignore', BiopythonWarning)
-parser = PDBParser()
-s = parser.get_structure("name", args['input_file'])
-model = s[0]
-chain = model[args['pdb_chain']]
-io = PDBIO()
-io.set_structure(chain)
-io.save(args['output_file'])
+# choose program
+if args['program'] == 1:
+    parser = PDBParser()
+    s = parser.get_structure("name", args['input'])
+    model = s[0]
+    chain = model[args['chain']]
+    io = PDBIO()
+    io.set_structure(chain)
+    io.save(''.join([args['input'].split(".")[0],"_",args['chain'],".pdb"]))
+else:
+    # import each fasta file from the working directory
+    for filename in sorted(os.listdir(str(os.getcwd()))):
+        if filename.endswith(".pdb"):
+    	    parser = PDBParser()
+    	    s = parser.get_structure("name", filename)
+    	    model = s[0]
+    	    chain = model[args['chain']]
+    	    io = PDBIO()
+    	    io.set_structure(chain)
+    	    io.save(''.join([filename.split(".")[0],"_",args['chain'],".pdb"]))
