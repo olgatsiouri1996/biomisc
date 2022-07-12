81cb97cffb52214105eb3ba4f12b4b7377366a65
Olga Tsiouri <olgatsiouri@outlook.com>
2022-07-12 19:53:33 +0300

    Update intersect_fasta.py

--- src/fasta_manipulation/intersect_fasta.py
+++ src/fasta_manipulation/intersect_fasta.py
@@ -7,7 +7,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-fa1", "--fasta1", required=True, help="input multi fasta file")
 ap.add_argument("-fa2", "--fasta2", required=True, help="input multi fasta file")
 ap.add_argument("-out", "--output", required=True, help="output multi fasta file")
-ap.add_argument("-pro", "--program", type=int, default=1, required=False, help="program to choose: 1. export the sequences of fasta1 that have the same identifier as fasta2, 2. export the sequences of fasta1 that doesn't have the same identifier as fasta2. Defaults to 1")
+ap.add_argument("-pro", "--program", type=int, default=1, required=False, help="program to choose: 1. export the sequences of fasta1 that have the same identifier as fasta2, 2. export the sequences of fasta1 that don't have the same identifier as fasta2. Defaults to 1")
 args = vars(ap.parse_args())
 # main
 # create function to split the input sequence based on a specific number of characters(60)
