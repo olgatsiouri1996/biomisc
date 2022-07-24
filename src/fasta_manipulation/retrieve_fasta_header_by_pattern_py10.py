f6f6a4261861bd2246e5975634c13c73132bf4e0
Olga Tsiouri <olgatsiouri@outlook.com>
2022-07-24 12:42:41 +0300

    Update retrieve_fasta_header_by_pattern_py10.py

--- src/fasta_manipulation/retrieve_fasta_header_by_pattern_py10.py
+++ src/fasta_manipulation/retrieve_fasta_header_by_pattern_py10.py
@@ -5,7 +5,7 @@ from pyfaidx import Fasta
 ap = argparse.ArgumentParser()
 ap.add_argument("-in", "--input", required=True, help="input multi-fasta file")
 ap.add_argument("-ma", "--match", required=True, type=str, help="word or phrase to search in each fasta header")
-ap.add_argument("-headers", "--headers", required=True, help=" 1 or 2-column txt file to save the output fasta headers")
+ap.add_argument("-headers", "--headers", required=True, help="1 or 2-column tab seperated txt file to save the output fasta identifiers or full fasta headers respectively")
 ap.add_argument("-pro", "--program", required=False, type=int, default=1, help="Program to choose. 1) collect only fasta identifiers, 2) collect full fasta headers. Default is 1")
 args = vars(ap.parse_args())
 # main
