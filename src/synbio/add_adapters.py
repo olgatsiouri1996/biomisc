32811c183c8cafaf450bfb123b411e5664ee4d6a
Olga Tsiouri <olgatsiouri@outlook.com>
2022-07-27 23:15:47 +0300

    file is indexed instead of storing all data to memory

--- src/synbio/add_adapters.py
+++ src/synbio/add_adapters.py
@@ -1,19 +1,22 @@
 # python3
+import sys
 import argparse
-from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
+from pyfaidx import Fasta
 # imput parameters
-ap = argparse.ArgumentParser(description = "add a left or right adapter or both in a single or multi-fasta file")
-ap.add_argument("-i", "--input", required=True,  help="input single or multi fasta file")
-ap.add_argument("-l", "--left", required=False, type=str, default="",  help="adapter to the left of the sequence. Default is no left adapter sequence to add")
-ap.add_argument("-r", "--right", required=False, type=str, default="",  help="adapter to the right of the sequence. Default is no right adapter sequence to add")
-ap.add_argument("-o", "--output", required=True,  help="output fasta file")
+ap = argparse.ArgumentParser(description = "add a left or right adapter or both by indexing a single or multi-fasta file")
+ap.add_argument("-in", "--input", required=True,  help="input single or multi fasta file")
+ap.add_argument("-le", "--left", required=False, type=str, default="",  help="adapter to the left of the sequence. Default is no left adapter sequence to add")
+ap.add_argument("-ri", "--right", required=False, type=str, default="",  help="adapter to the right of the sequence. Default is no right adapter sequence to add")
+ap.add_argument("-out", "--output", required=True, help="output single or multi-fasta file")
 args = vars(ap.parse_args())
 # main
-records = [] # setup an empty list
-for record in SeqIO.parse(args['input'], "fasta"):
-    # add this record to the list
-    records.append(SeqRecord(Seq(''.join([args['left'],str(record.seq),args['right']])),id=record.id,description=record.description))
-# export to fasta
-SeqIO.write(records, args['output'], "fasta")
+# create function to split the input sequence based on a specific number of characters(60)
+def split_every_60(s): return [str(s)[i:i+60] for i in range(0,len(str(s)),60)]
+# import fasta file
+features = Fasta(args['input'])
+# merge sequences and export to fasta
+sys.stdout = open(args['output'], 'a')
+for key in features.keys():
+    print(''.join([">",features[key].long_name]))
+    print('\n'.join(split_every_60(args['left'] + features[key][:].seq + args['right'])))
+sys.stdout.close()
