c4112a45aecc6a7cc2445442163f7f9d65e58858
Olga Tsiouri <olgatsiouri@outlook.com>
2022-07-31 00:37:33 +0300

    description fixed

---
+++ src/fasta_manipulation/fasta_to_ids_or_headers_py10.py
@@ -0,0 +1,25 @@
+# python3
+import argparse
+from pyfaidx import Fasta
+# input arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-in", "--input", required=True, help="input fasta file")
+ap.add_argument("-txt", "--txt", required=True, help="1 or 2-column txt file to save the output fasta identifiers or full fasta headers with identifier and description respectively")
+ap.add_argument("-desc", "--description", required=False, type=str, default='F', help="Collect fasta description along with fasta identifiers? Default is F")
+args = vars(ap.parse_args())
+# main
+# index multi-fasta file
+features = Fasta(args['input'])
+# choose program
+program = args['description']
+match program:
+    case 'F':
+        # export to 1-column txt file
+        with open(args['txt'], 'w') as filehandle:
+            for key in features.keys():
+                filehandle.write('%s\n' % key)
+    case 'T':
+        # export to 1-column txt file
+        with open(args['txt'], 'w') as filehandle:
+            for key in features.keys():
+                filehandle.write('%s\n' % '\t'.join([key,str(features[key].long_name).split(key)[1]]))
