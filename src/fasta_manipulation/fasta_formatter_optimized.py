2c05f0eaacdf5eaf5c4dec93ded2fc9192e628f6
Olga Tsiouri <olgatsiouri@outlook.com>
2022-08-15 02:49:29 +0300

    Update fasta_formatter_optimized.py

--- src/fasta_manipulation/fasta_formatter_optimized.py
+++ src/fasta_manipulation/fasta_formatter_optimized.py
@@ -3,20 +3,20 @@ import argparse
 from pyfaidx import Fasta
 import sys
 # input parameters
-ap = argparse.ArgumentParser(description="indexes the input fasta for memory efficiency file and changes the width of fasta sequences")
+ap = argparse.ArgumentParser(description="indexes the input fasta file for memory efficiency and changes the width of fasta sequences")
 ap.add_argument("-in", "--input", required=True, help="input fasta file")
 ap.add_argument("-out", "--output", required=True, help="output fasta file")
 ap.add_argument("-width", "--width", required=False, type=int, default=80, help="number of characters per line. Default is  80")
 args = vars(ap.parse_args())
 # main
 # create function to split the input sequence based on a specific number of characters
-def split_every_width(s): return [s[i:i+ args['width'] for i in range(0,len(s),args['width'])]
+def split_every_width(s): return [s[i:i+ args['width']] for i in range(0,len(s),args['width'])]
 # import fasta file
 features = Fasta(args['input'])
 # extract a fasta file with a new line width
 sys.stdout = open(args['output'], 'a')
 for key in features.keys():
-    print(''.join([">",features[key].long_name]))
+    print(''.join([">",features[key].long_name]).replace('\r',''))
     print('\n'.join(split_every_width(features[key][:].seq)))
 sys.stdout.close()
 
