76dca3388ff31fd402d7e9a4349a02b494f9e8c1
Olga Tsiouri <olgatsiouri@outlook.com>
2022-08-15 20:44:11 +0300

    Update intersect_fasta.py

--- src/fasta_manipulation/intersect_fasta.py
+++ src/fasta_manipulation/intersect_fasta.py
@@ -7,7 +7,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-fa1", "--fasta1", required=True, help="input multi fasta file")
 ap.add_argument("-fa2", "--fasta2", required=True, help="input multi fasta file")
 ap.add_argument("-out", "--output", required=True, help="output multi fasta file")
-ap.add_argument("-pro", "--program", type=int, default=1, required=False, help="program to choose: 1. export the sequences of fasta1 that have the same identifier as fasta2, 2. export the sequences of fasta1 that don't have the same identifier as fasta2. Defaults to 1")
+ap.add_argument("-pro", "--program", type=int, default=1, required=False, help="program to choose: 1. export the sequences of fasta2 that have the same identifiers as fasta1, 2. export the sequences of fasta1 that don't have the same identifiers as fasta2")
 args = vars(ap.parse_args())
 # main
 # create function to split the input sequence based on a specific number of characters(60)
@@ -22,8 +22,8 @@ if args['program'] == 1:
     # iterate the following 2 lists
     for key in features1.keys():
         if key in features2.keys():
-            print(''.join([">",features1[key].long_name]))
-            print('\n'.join(split_every_60(features1[key][:].seq)))
+            print(''.join([">",features2[key].long_name]).replace('\r',''))
+            print('\n'.join(split_every_60(features2[key][:].seq)))
     sys.stdout.close()
 else:
     # export to fasta
@@ -31,6 +31,7 @@ else:
     # iterate the following 2 lists
     for key in features1.keys():
         if key not in features2.keys():
-            print(''.join([">",features1[key].long_name]))
+            print(''.join([">",features1[key].long_name]).replace('\r',''))
             print('\n'.join(split_every_60(features1[key][:].seq)))
     sys.stdout.close()
+
