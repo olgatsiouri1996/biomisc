67543e2029a388cae7d9d87cf1595910b8c929d6
olgatsiouri1996 <olgatsiouri@outlook.com>
2022-08-19 16:32:22 +0300

    Update split_fasta_into_chunks.py

--- src/fasta_manipulation/split_fasta_into_chunks.py
+++ src/fasta_manipulation/split_fasta_into_chunks.py
@@ -12,16 +12,13 @@ args = vars(ap.parse_args())
 # main
 # create function to split the input sequence based on a specific number of characters(60)
 def split_every_60(s): return [str(s)[i:i+60] for i in range(0,len(str(s)),60)]
-# create function to slice based on window and step
-def slice_by_winstep(rec):
-    for i in range(0, features[rec][:].end - args['window'] + 1, args['step']):
-        print(''.join([">",rec,"_start_",str(i+1),"_stop_",str(i + args['window'])]).replace('\r',''))
-        print('\n'.join(split_every_60(features[rec][i:i + args['window']].seq)))
-    return
 # import multi or single-fasta file
 features = Fasta(args['input'])
 # iterate input headers to extract sequences and export as multi-fasta
 sys.stdout = open(args['output'], 'a')
 for key in features.keys():
-    slice_by_winstep(key)
+    # slice based on window and step
+    for i in range(0, features[key][:].end - args['window'] + 1, args['step']):
+        print(''.join([">",key,"_start_",str(i+1),"_stop_",str(i + args['window'])]).replace('\r',''))
+        print('\n'.join(split_every_60(features[key][i:i + args['window']].seq)))
 sys.stdout.close()
