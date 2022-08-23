860fdd638b3f4852d5d83555ce334ffa54c18cbf
olgatsiouri1996 <olgatsiouri@outlook.com>
2022-08-24 01:35:54 +0300

    Update fasta_to_tab.py

--- src/fasta_manipulation/fasta_to_tab.py
+++ src/fasta_manipulation/fasta_to_tab.py
@@ -4,7 +4,7 @@ import argparse
 import pyfastx
 # input parameters
 ap = argparse.ArgumentParser()
-ap.add_argument("-in","--input", required=False, help="input multi-fasta file(all fasta records should either have no fasta description or all of them should have fasta description)")
+ap.add_argument("-in","--input", required=True, help="input multi-fasta file(all fasta records should either have no fasta description or all of them should have fasta description)")
 ap.add_argument("-pro", "--program", required=False, default=1, type=int, help="output to choose: 1) 2-column txt file with fasta identifiers and fasta sequences 2) 2-column txt file with full fasta headers and fasta sequences 3) 3-column txt file with fasta identifiers, fasta descriptions and fasta sequences. Default is 1")
 ap.add_argument("-out","--output", required=True, help="output txt file")
 args = vars(ap.parse_args())
