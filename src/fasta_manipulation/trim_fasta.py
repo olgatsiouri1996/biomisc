727fa64bf553c5af28cb8e255e7f319ec8bc94bd
Olga Tsiouri <olgatsiouri@outlook.com>
2022-10-16 22:37:22 +0300

    output single fasta files contain start and end location

--- src/fasta_manipulation/trim_fasta.py
+++ src/fasta_manipulation/trim_fasta.py
@@ -3,12 +3,13 @@ import os
 import argparse
 from Bio import SeqIO
 # input parameters
-ap = argparse.ArgumentParser()
-ap.add_argument("-in", "--input", required=False, help="input fasta file")
-ap.add_argument("-start", "--start", required=False, default=1, type=int, help="region to start writing the fasta file(default 1)")
+ap = argparse.ArgumentParser() 
+ap.add_argument("-in", "--input", required=False , help="input fasta file")
+ap.add_argument("-start", "--start", required=False, default=1, type=int, help="region to start writing the fasta file. Default is 1")
 ap.add_argument("-stop", "--stop", required=False, type=int, help="region to stop writing the fasta file(it can be both a positive and  a negative number)")
-ap.add_argument("-pro", "--program", required=False,default=1, type=int, help="program to choose 1) add both start and stop location 2) the stop location will be that of the sequence length. Default is 1")
-ap.add_argument("-type", "--type", required=False,default=1, type=int, help="type of fasta to import 1) 1 multi-fasta file 2)  many single-fasta files. Default is 1")
+ap.add_argument("-dir", "--directory", required=False, type=str, help="directory to search for fasta files")
+ap.add_argument("-pro", "--program", required=False,default=1, type=int, help="program to choose: 1) add both start and stop location 2) the stop location will be that of the sequence length. Default is 1")
+ap.add_argument("-type", "--type", required=False,default=1, type=int,  help="type of fasta to import: 1) 1 multi-fasta file 2) many single-fasta files. Default is 1")
 ap.add_argument("-out", "--output", required=False, help="output fasta file")
 args = vars(ap.parse_args())
 # main
@@ -32,8 +33,7 @@ def fastatrim(fastarec,fastaseq):
             print("-start parameter must be a positive integer")
             exit(1)
         # add end parameter according to program 2
-        args['stop'] = len(fastaseq)
-        seq_end = args['stop']
+        seq_end = len(fastaseq)
     # subset each fasta record
     return fastarec[seq_start:seq_end]
 # choose fasta type to import
@@ -48,12 +48,17 @@ if args['type'] == 1:
     SeqIO.write(sequences, args['output'], "fasta")
 else:
     # import each fasta file from the working directory
-    for filename in sorted(os.listdir(str(os.getcwd()))):
+    for filename in sorted(os.listdir(os.chdir(args['directory']))):
         if filename.endswith(".fa") or filename.endswith(".fasta"):
             # read each file, trim and create SeqRecord to export
             record = SeqIO.read(filename, "fasta")
             sequence = fastatrim(record,record.seq)
-            # export to fasta
-            SeqIO.write(sequence, "".join([filename.split(".")[0],"_","trimmed",".fasta"]), "fasta")
-
+            if args['program'] ==1:
+                if args['stop'] > 0:
+                    # export to fasta
+                    SeqIO.write(sequence, "".join([filename.split(".")[0],"_",str(args['start']),"_",str(args['stop']),".fasta"]), "fasta")
+                else:
+                    SeqIO.write(sequence, "".join([filename.split(".")[0],"_",str(args['start']),"_",str(len(sequence.seq)),".fasta"]), "fasta")
+            else:
+                SeqIO.write(sequence, "".join([filename.split(".")[0],"_",str(args['start']),"_",str(len(record.seq)),".fasta"]), "fasta")
 
