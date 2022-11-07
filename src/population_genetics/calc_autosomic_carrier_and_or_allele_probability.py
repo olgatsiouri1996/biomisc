3d79ffb1a41fead8e6371128d24ed975cb1f719c
Olga Tsiouri <olgatsiouri@outlook.com>
2022-11-07 15:52:06 +0200

    Create calc_autosomic_carrier_and_or_allele_probability.py

---
+++ src/population_genetics/calc_autosomic_carrier_and_or_allele_probability.py
@@ -0,0 +1,31 @@
+# python3
+import argparse
+import sys
+# input parameters
+ap = argparse.ArgumentParser(description='calculate the probability of an individual carrying at least 1 autosomic allele copy and/or being heterozygous, using the proportion of homozygous individuals in a given population')
+ap.add_argument("-in", "--input", required=True,  help="1-column input txt file with the proportion of individuals homozygous for the specific allele")
+ap.add_argument("-type", "--type", required=False, type=str, default='allele', help="type of calculation: allele, carrier, both. Default is allele")
+ap.add_argument("-out", "--output", required=True, help="2 or 3-column tab-seperated output txt file with the probability of containing an allele and/or being heterozygous and the input column")
+args = vars(ap.parse_args())
+# convert 1-column txt file to list
+with open(args['input'],'r') as f:
+    A = f.readlines()
+A = [float(x.strip()) for x in A]
+# choose and output based 
+sys.stdout = open(args['output'],'w')
+if args['type'] == 'allele':
+    print('probability_of_allele','proportion_of_homozygous_individuals',sep='\t',end='\n')
+    for i in A:
+        print("%.3f" % round(2*i**0.5-i,3),"%.3f" % round(i,3),sep='\t',end='\n')
+elif args['type'] == 'carrier':
+    print('probability_of_carriers','proportion_of_homozygous_individuals',sep='\t',end='\n')
+    for i in A:
+        p = i**0.5
+        print("%.3f" % round(2*p*(1-p),3),"%.3f" % round(i,3),sep='\t',end='\n')
+else:
+    print('probability_of_allele','probability_of_carriers','proportion_of_homozygous_individuals',sep='\t',end='\n')
+    for i in A:
+        p = i**0.5
+        print("%.3f" % round(2*i**0.5-i,3),"%.3f" % round(2*p*(1-p),3),"%.3f" % round(i,3),sep='\t',end='\n')
+
+sys.stdout.close()
