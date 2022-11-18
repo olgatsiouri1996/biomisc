cf6b7eba1850bf34bec68c33a30c5a898fe7120e
Olga Tsiouri <olgatsiouri@outlook.com>
2022-11-18 02:04:09 +0200

    allele frequency column added

--- src/population_genetics/calc_autosomic_carrier_and_or_allele_probability.py
+++ src/population_genetics/calc_autosomic_carrier_and_or_allele_probability.py
@@ -2,16 +2,14 @@
 import argparse
 import sys
 # input parameters
-ap = argparse.ArgumentParser(description='calculate the probability of an individual carrying at least 1 autosomic allele copy and/or being heterozygous, using the proportion of homozygous individuals in a given population')
+ap = argparse.ArgumentParser(description='calculate the probability of an individual carrying at least 1 autosomic allele copy and/or being heterozygous, using the probability of homozygous individuals in a given diploid population')
 ap.add_argument("-in", "--input", required=True,  help="1-column input txt file with the proportion of individuals homozygous for the specific allele")
 ap.add_argument("-type", "--type", required=False, type=str, default='allele', help="type of calculation: allele, carrier, both. Default is allele")
 ap.add_argument("-all", "--allele", required=False, type=str, default='this', help="allele to calculate the probability of for each calculation type. Options: this, other. Default is this")
-ap.add_argument("-out", "--output", required=True, help="2 or 3-column tab-seperated output txt file with the probability of containing an allele and/or being heterozygous and the input column(or the proportion of homozygous individuals for the other allele)")
+ap.add_argument("-out", "--output", required=True, help="3 or 4-column tab-seperated output txt file with the allele frequency, the probability of containing an allele and/or being heterozygous and the input column(for this or the other allele)")
 args = vars(ap.parse_args())
 # convert 1-column txt file to list
-with open(args['input'],'r') as f:
-    A = f.readlines()
-A = (float(x.strip()) for x in A)
+A = (float(line.rstrip()) for line in open(args['input']))
 if args['allele'] == 'this':
     pass
 else:
@@ -19,18 +17,18 @@ else:
 # choose an output based on the calculation type
 sys.stdout = open(args['output'],'w')
 if args['type'] == 'allele':
-    print('probability_of_allele','proportion_of_homozygous_individuals',sep='\t',end='\n')
+    print('probability_of_containing_allele','probability_of_homozygous_individuals','allele_frequency',sep='\t',end='\n')
     for i in A:
-        print("%.3f" % round(2*i**0.5-i,3),"%.3f" % round(i,3),sep='\t',end='\n')
+        print("%.3f" % round(2*i**0.5-i,3),"%.3f" % round(i,3),"%.3f" % round(i**0.5,3),sep='\t',end='\n')
 elif args['type'] == 'carrier':
-    print('probability_of_carriers','proportion_of_homozygous_individuals',sep='\t',end='\n')
+    print('probability_of_carriers','probability_of_homozygous_individuals','allele_frequency',sep='\t',end='\n')
     for i in A:
         p = i**0.5
-        print("%.3f" % round(2*p*(1-p),3),"%.3f" % round(i,3),sep='\t',end='\n')
+        print("%.3f" % round(2*p*(1-p),3),"%.3f" % round(i,3),"%.3f" % round(p,3),sep='\t',end='\n')
 else:
-    print('probability_of_allele','probability_of_carriers','proportion_of_homozygous_individuals',sep='\t',end='\n')
+    print('probability_of_containing_allele','probability_of_carriers','probability_of_homozygous_individuals','allele_frequency',sep='\t',end='\n')
     for i in A:
         p = i**0.5
-        print("%.3f" % round(2*i**0.5-i,3),"%.3f" % round(2*p*(1-p),3),"%.3f" % round(i,3),sep='\t',end='\n')
+        print("%.3f" % round(2*i**0.5-i,3),"%.3f" % round(2*p*(1-p),3),"%.3f" % round(i,3),"%.3f" % round(p,3),sep='\t',end='\n')
 
 sys.stdout.close()
