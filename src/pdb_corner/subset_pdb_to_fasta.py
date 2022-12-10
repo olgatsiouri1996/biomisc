ef22594b5b6d60e6ffe56a6c1d8f6ddffb051f09
olgatsiouri1996 <olgatsiouri@outlook.com>
2022-12-11 00:33:16 +0200

    use biopandas instead of biopython so an intermediate file isn't needed

--- src/pdb_corner/subset_pdb_to_fasta.py
+++ src/pdb_corner/subset_pdb_to_fasta.py
@@ -1,69 +1,59 @@
 # python3
 import os
+import sys
 import argparse
-from Bio.PDB import *
-from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
+from biopandas.pdb import PandasPdb
 # input parameters
-ap = argparse.ArgumentParser()
-ap.add_argument("-pdb", "--pdb", required=False, help="input pdb file")
-ap.add_argument("-model", "--model",default=0, required=False, help="model from pdb file to select(integer). Default is 0(1 model only)")
-ap.add_argument("-chain", "--chain", required=False, default='A', help="chain from pdb file to select")
-ap.add_argument("-start", "--start", required=False,default=1, type=int, help="amino acid in chain to start writing the fasta file. Default is 1")
+ap = argparse.ArgumentParser(description="converts a pdb file into a fasta file based on the chain, start and end locations")
+ap.add_argument("-in", "--input", required=False, help="input pdb file")
+ap.add_argument("-dir", "--directory", required=False, type=str, default='.', help="directory to search for pdb files(the directory can contain many filetypes). Default is the current directory")
+ap.add_argument("-chain", "--chain", required=False, default='A', help="chain from pdb file to select.Default is A")
+ap.add_argument("-start", "--start", required=False, default=1, type=int, help="amino acid in chain to start writing the fasta file. Default is 1")
 ap.add_argument("-end", "--end", required=False, type=int, help="amino acid in chain to end writing the fasta file")
-ap.add_argument("-pro", "--program", required=False,default=1, type=int, help="program to choose 1) add both start and end location 2) the end location will be that of the latest amino acid in the chain. Default is 1")
-ap.add_argument("-type", "--type", required=False,default=1, type=int, help="type of input to choose. 1) 1 pdb file, 2) many pdb files. Default is 1")
+ap.add_argument("-type", "--type", required=False,default=1, type=int, help="type of input to choose: 1) 1 pdb file, 2) many pdb files. Defaults to 1")
+ap.add_argument("-pro", "--program", required=False,default=1, type=int,help="program to choose: 1) add both start and end location 2) the end location will be that of the latest amino acid in the chain. Defaults to 1")
 args = vars(ap.parse_args())
 # main
+# create function to split the input sequence based on a specific number of characters(60)
+def split_every_60(s): return [str(s)[i:i+60] for i in range(0,len(str(s)),60)]
 # create function to trim + convert to fasta
 def trim_to_fasta(fi):
-    # select chain
-    fill = s[int(args['model'])][args['chain']]
-    # select between importing 1 or many pdb files
-    if args['type'] == 1:
-        # retrieve the pdb id of the input file
-        pdb_id = str(fi).split(".")[0]
-    else:
-        pdb_id = fi.split(".")[0]
-    # retrieve chain amino acids
-    ppb = PPBuilder()
-    for pp in ppb.build_peptides(fill):
-        aa_chain = str(pp.get_sequence())
-    # choose program
+    # insert pdb file
+    ppdb = PandasPdb()
+    ppdb.read_pdb(fi)
+    # convert 3 letters aa to 1
+    one = ppdb.amino3to1()
+    # select chain and convert to string
+    sequence = ''.join(one.loc[one['chain_id']==args['chain'],'residue_name'])
+    # choose end location
     if args['program'] == 1:
-        # fix the index for start parameter
-        if args['start'] > 0:
-            aa_start = args['start'] -1
-        else:
-            print("-start parameter must be a positive integer")
-            exit(1)
-        # add end parameter
-        aa_end = args['end']
+        seq_end = args['end']
     else:
-        # fix the index for start parameter
-        if args['start'] > 0:
-            aa_start = args['start'] -1
-        else:
-            print("-start parameter must be a positive integer")
-            exit(1)
-        # add end parameter according to program 2
-        args['end'] = len(aa_chain)
-        aa_end = args['end']
-    # subset based on aa in chain
-    sub_seq = aa_chain[aa_start:aa_end]
+        seq_end = len(sequence)
+    # subset by location
+    prot = sequence[int(args['start'] -1):seq_end]
+    # remove dataframes and lists
+    del ppdb; del one
+    if args['type'] ==1:
+        # get pdb file basename
+        basename = str(os.path.basename(fi)).split('.')[0]
+    else:
+        basename = str(fi).split('.')[0]
+    # if 1 pdb file only select output directory
+    if args['type'] == 1:
+        os.chdir(os.path.dirname(fi))
+    else:
+        pass
     # export to fasta
-    record = SeqRecord(Seq(sub_seq),id="".join([str(pdb_id),"_",str(args['chain']),"_",str(args['start']),"_",str(args['end'])]),description="")
-    return SeqIO.write(record, "".join([str(pdb_id),"_",str(args['chain']),"_",str(args['start']),"_",str(args['end']),".fasta"]), "fasta")
+    sys.stdout = open(''.join([basename,"_",args['chain'],"_",str(args['start']),"_",str(seq_end),".fasta"]), 'a')
+    print(''.join([">",basename,"_",args['chain'],"_",str(args['start']),"_",str(seq_end)]).replace('\r',''))
+    print('\n'.join(split_every_60(prot)))
+    sys.stdout.close()
 # select between importing 1 or many pdb files
-if args['type'] == 1:    
-    parser = PDBParser()
-    s = parser.get_structure("name", args['pdb'])
-    trim_to_fasta(args['pdb'])
+if args['type'] == 1:
+    trim_to_fasta(args['input'])
 else:
-    # import each fasta file from the working directory
-    for filename in sorted(os.listdir(str(os.getcwd()))):
+     # import each pdb file from the working directory
+    for filename in sorted(os.listdir(os.chdir(args['directory']))):
         if filename.endswith(".pdb"):
-            parser = PDBParser()
-            s = parser.get_structure("name", filename)
-            trim_to_fasta(filename)
+           trim_to_fasta(filename)
