e8495ed9453b6e9eb01bb37f8bfba578601c1dab
Olga Tsiouri <olgatsiouri@outlook.com>
2023-06-03 23:58:52 +0300

    faster algorithm for subsetting

--- src/fasta_manipulation/intersect_fasta.py
+++ src/fasta_manipulation/intersect_fasta.py
@@ -1,37 +1,28 @@
 # python3
-import sys
 import argparse
 from pyfaidx import Fasta
 # imput parameters
 ap = argparse.ArgumentParser()
 ap.add_argument("-fa1", "--fasta1", required=True, help="input multi fasta file")
 ap.add_argument("-fa2", "--fasta2", required=True, help="input multi fasta file")
+ap.add_argument("-pro", "--program", type=int, default=1, required=False, help="program to choose: 1. export the sequences of fasta1 that have the same identifiers as fasta2, 2. export the sequences of fasta1 that don't have the same identifiers as fasta2. Default is 1")
 ap.add_argument("-out", "--output", required=True, help="output multi fasta file")
-ap.add_argument("-pro", "--program", type=int, default=1, required=False, help="program to choose: 1. export the sequences of fasta2 that have the same identifiers as fasta1, 2. export the sequences of fasta1 that don't have the same identifiers as fasta2")
 args = vars(ap.parse_args())
 # main
-# create function to split the input sequence based on a specific number of characters(60)
-def split_every_60(s): return [str(s)[i:i+60] for i in range(0,len(str(s)),60)]
+# helper function to wrap fasta sequence to 60 characters per line
+def wrap_fasta_seq(seq):
+    return '\n'.join([seq[i:i+60] for i in range(0, len(seq), 60)])
 # create fasta index
 features1 = Fasta(args['fasta1'])
 features2 = Fasta(args['fasta2'])
-# choose program
+## choose program
 if args['program'] == 1:
-    # export to fasta
-    sys.stdout = open(args['output'], 'a')
-    # iterate the following 2 lists
-    for key in features1.keys():
-        if key in features2.keys():
-            print(''.join([">",features2[key].long_name]).replace('\r',''))
-            print('\n'.join(split_every_60(features2[key][:].seq)))
-    sys.stdout.close()
+    # find common ids of the 2 files
+    final = (set(features1.keys()).intersection(features2.keys()))
 else:
-    # export to fasta
-    sys.stdout = open(args['output'], 'a')
-    # iterate the following 2 lists
-    for key in features1.keys():
-        if key not in features2.keys():
-            print(''.join([">",features1[key].long_name]).replace('\r',''))
-            print('\n'.join(split_every_60(features1[key][:].seq)))
-    sys.stdout.close()
+    final = (set(features1.keys()).difference(features2.keys()))
+# export to fasta
+with open(args['output'], 'w') as f:
+    for fin in final:
+        f.write(f'>{str(features1[str(fin)].long_name).rstrip()}\n{wrap_fasta_seq(features1[str(fin)][:].seq)}\n')
 
